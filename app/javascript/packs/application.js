b0aa60e25dd413d47e74476599632678d3d300bb
Alex Fiennes <alexfiennes246@gmail.com>
2019-04-19 17:03:54 +0100

    Improved banner, added taglines and languages sections to homepage

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,2 +1,23 @@
 import "bootstrap";
 
+const typed = new Typed('#typed',{
+      strings: ['Learn a language from a local expert', 'Redistribute wealth to a local expert', 'Learn a language from a local expert'],
+      backSpeed: 30,
+      typeSpeed: 40,
+      backDelay: 1500,
+});
+
+$(window).scroll(function() {
+  if ($(document).scrollTop() >= $(window).height() - 64) {
+    $('.navbar').addClass('fill-navbar');
+  } else {
+    $('.navbar').removeClass('fill-navbar');
+  }
+});
+
+$('#arrow').on('click', function(event){
+  $('html, body').animate({
+    scrollTop: $("#arrow-scroll-anchor").offset().top
+  }, 800);
+
+});
